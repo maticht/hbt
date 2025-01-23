2502c727b450587db721b0d73f870cf5c1fde36b
maticht <74956969+maticht@users.noreply.github.com>
2025-01-23 20:26:20 +0100

    new link img

--- src/components/Motka.jsx
+++ src/components/Motka.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './style.css';
 
 const Motka = ({ value, onClose }) => {
+    const [displayedText, setDisplayedText] = useState('');
+    const [isButtonVisible, setIsButtonVisible] = useState(false);
 
     const slides = [
         {
             key: 'hello',
-            text: 'Привет! Я Мотька. Приглашаю тебя пройти этот тест!',
+            text: 'Привет! Приглашаю тебя пройти этот тест!',
             image: '/img/hello.png',
             buttonText: 'Продолжить',
         },
@@ -24,7 +26,7 @@ const Motka = ({ value, onClose }) => {
         },
         {
             key: 'rules',
-            text: 'Ну, на самом деле каждый может назваться Танюсичкой, а ты докажи, что ты действительно та самая!',
+            text: 'Ну, на самом деле каждый может назваться Танюшей, а ты докажи, что ты действительно та самая!',
             image: '/img/info.png',
             buttonText: 'Начать игру!',
         },
@@ -36,7 +38,7 @@ const Motka = ({ value, onClose }) => {
         },
         {
             key: 'yesYears',
-            text: 'Да да, все так, моя именинница)',
+            text: 'Да-да, все так, моя именинница)',
             image: '/img/yes.png',
             buttonText: 'Завершить',
         },
@@ -74,12 +76,43 @@ const Motka = ({ value, onClose }) => {
 
     const slide = slides.find((slide) => slide.key === value) || slides[1];
 
+    useEffect(() => {
+        setDisplayedText(''); // Очищаем текст перед началом
+        setIsButtonVisible(false); // Скрываем кнопку
+
+        const characters = Array.from(slide.text); // Преобразуем строку в массив символов
+        let index = 0;
+
+        characters.map((char, i) => {
+            setTimeout(() => {
+                setDisplayedText((prev) => prev + char); // Добавляем текущий символ
+                if (i === characters.length - 1) { // Когда последний символ добавлен
+                    setIsButtonVisible(true); // Показываем кнопку
+                }
+            }, 50 * i); // Увеличиваем задержку для каждого символа
+        });
+
+        return () => {
+            setDisplayedText('');
+            setIsButtonVisible(false);
+        };
+    }, [slide.text]);
+
+
+
+
     return (
         <div className="motka-overlay">
             <div className="motka-modal">
                 <div className="motka-content-block">
-                    <p>{slide.text}</p>
-                    <button onClick={onClose} className="check-button">{slide.buttonText}</button>
+                    <p>{displayedText}</p>
+                    <button
+                        onClick={onClose}
+                        className={`check-button ${isButtonVisible ? 'visible' : 'hidden'}`}
+                        disabled={!isButtonVisible}
+                    >
+                        {slide.buttonText}
+                    </button>
                 </div>
                 <div className="motka-image-wrapper">
                     <img
